24f086319bac6315cbf7f2a064ef7b9a6c7abe61
m0onsoo <kd.mpark10@gmail.com>
2026-03-10 16:06:50 -0700

    TLE 코드 추가

--- week21/239_moon.py
+++ week21/239_moon.py
@@ -15,3 +15,48 @@ class Solution:
                 res.append(q[0])
         
         return res
+
+
+"""
+# TLE Solution:
+
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        
+        # res = []
+        # for i in range(n - k + 1):
+        #     window = nums[i: i + k]
+
+        #     res.append(max(window))
+        
+        res = []
+        window = deque()
+        max_num = nums[0]
+        for i in range(n):
+            # print(window, max_num)
+            if len(window) < k:
+                # O(1)
+                window.append(nums[i])
+                max_num = max(max_num, nums[i])
+            else:
+                # res array update
+                res.append(max_num)
+
+                # update sliding window
+                pop = window.popleft()
+                window.append(nums[i])
+
+                # update max_num
+                if nums[i] > max_num:
+                    max_num = nums[i]
+                elif pop == max_num:
+                    # O(k)
+                    max_num = max(window)
+        res.append(max_num)
+        
+        return res
+
+"""
